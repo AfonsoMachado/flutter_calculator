5313a16617f9caca864bb561dd9af91dc2b9aba5
Afonso Machado <afonsosmachado@gmail.com>
2023-09-22 12:56:49 -0300

    feat: styling button colors

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
-  final String text;
+  static const DARK = Color.fromRGBO(82, 82, 82, 1);
+  static const DEFAULT = Color.fromRGBO(112, 112, 112, 1);
+  static const OPERATION = Color.fromRGBO(250, 158, 13, 1);
 
-  const Button({super.key, required this.text});
+  final String text;
+  final bool big;
+  final Color color;
+
+  const Button(
+      {super.key, required this.text, this.big = false, this.color = DEFAULT});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text(text),
+    return Expanded(
+      flex: big ? 2 : 1,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: color,
+            textStyle: const TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.w300)),
+        onPressed: () {},
+        child: Text(text),
+      ),
     );
   }
 }
--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -1,12 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calculator/components/button.dart';
+import 'package:flutter_calculator/components/button_row.dart';
 
 class Keyboard extends StatelessWidget {
   const Keyboard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return const SizedBox(
       height: 500,
+      child: Column(
+        children: [
+          ButtonRow([
+            Button(
+              text: 'AC',
+              big: true,
+            ),
+            Button(
+              text: '%',
+            ),
+            Button(
+              text: '/',
+            ),
+          ]),
+          ButtonRow([
+            Button(
+              text: '7',
+            ),
+            Button(
+              text: '8',
+            ),
+            Button(
+              text: '9',
+            ),
+            Button(
+              text: 'x',
+            ),
+          ]),
+          ButtonRow([
+            Button(
+              text: '4',
+            ),
+            Button(
+              text: '5',
+            ),
+            Button(
+              text: '6',
+            ),
+            Button(
+              text: '-',
+            ),
+          ]),
+          ButtonRow([
+            Button(
+              text: '1',
+            ),
+            Button(
+              text: '2',
+            ),
+            Button(
+              text: '3',
+            ),
+            Button(
+              text: '+',
+            ),
+          ]),
+          ButtonRow([
+            Button(
+              text: '0',
+              big: true,
+            ),
+            Button(
+              text: '.',
+            ),
+            Button(
+              text: '=',
+            ),
+          ]),
+        ],
+      ),
     );
   }
 }
