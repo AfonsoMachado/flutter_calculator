a65460896a09412f5ad9c37a3028df98fff9db68
Afonso Machado <afonsosmachado@gmail.com>
2023-09-22 15:22:57 -0300

    feat: show command on display

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,1 +1,11 @@
-class Memory {}
+class Memory {
+  String _value = '0';
+
+  String get value {
+    return _value;
+  }
+
+  void applyCommand(String command) {
+    _value += command;
+  }
+}
--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calculator/components/display.dart';
 import 'package:flutter_calculator/components/keyboard.dart';
+import 'package:flutter_calculator/models/memory.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
   const Calculator({super.key});
 
-  _onPressed(String text) {
-    print(text);
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Column(
-        children: [const Display('1123'), Keyboard(_onPressed)],
+        children: [Display(memory.value), Keyboard(_onPressed)],
       ),
     );
   }
