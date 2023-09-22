c29528570c2ebd002b33a277597ec7c8ffbe4680
Afonso Machado <afonsosmachado@gmail.com>
2023-09-22 16:52:16 -0300

    feat: add calculate operations

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -4,7 +4,7 @@ class Memory {
   String _value = '0';
   final _buffer = [0.0, 0.0];
   int _bufferIndex = 0;
-  late String operation;
+  String? _operation;
   bool _wipeValue = false;
 
   String get value {
@@ -12,7 +12,24 @@ class Memory {
   }
 
   _setOperation(String newOperation) {
-    _wipeValue = true;
+    bool isEqualSign = newOperation == '=';
+
+    if (_bufferIndex == 0) {
+      if (!isEqualSign) {
+        _operation = newOperation;
+        _bufferIndex = 1;
+        _wipeValue = true;
+      }
+    } else {
+      _buffer[0] = _calculate();
+      _buffer[1] = 0.0;
+      _value = _buffer[0].toString();
+      _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
+
+      _operation = isEqualSign ? null : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1;
+    }
+    _wipeValue = !isEqualSign;
   }
 
   _addDigit(String digit) {
@@ -27,10 +44,32 @@ class Memory {
     _wipeValue = false;
 
     _buffer[_bufferIndex] = double.tryParse(_value) ?? 0.0;
+    print(_buffer);
   }
 
   _allClear() {
     _value = '0';
+    _buffer.setAll(0, [0.0, 0.0]);
+    _operation = null;
+    _bufferIndex = 0;
+    _wipeValue = false;
+  }
+
+  _calculate() {
+    switch (_operation) {
+      case '%':
+        return _buffer[0] % _buffer[1];
+      case '/':
+        return _buffer[0] / _buffer[1];
+      case 'x':
+        return _buffer[0] * _buffer[1];
+      case '-':
+        return _buffer[0] - _buffer[1];
+      case '+':
+        return _buffer[0] + _buffer[1];
+      default:
+        return _buffer[0];
+    }
   }
 
   void applyCommand(String command) {
